754cf03a2106867bb556ab848eb9d5777c616f70
momeemt <momiimt@gmail.com>
2020-04-29 07:13:44 +0900

    ✨ マッチ中の変数束縛(2.4)

--- tutorial/lib/tutorial.ex
+++ tutorial/lib/tutorial.ex
@@ -1,6 +1,24 @@
 defmodule Tutorial do
   def main([]) do
-    IO.puts(patternMatching1)
+    IO.puts(patternMatching3)
+  end
+
+  def patternMatching3() do
+    a = 2
+    # [a, b, a] = [1, 2, 3]
+    # [a, b, a] = [1, 1, 2]
+    a = 1
+    # ^a = 2
+    ^a = 1
+    ^a = 2 - a
+    a
+  end
+
+  def patternMatching2() do
+    # [a, b, a] = [1, 2, 3]
+    # [a, b, a] = [1, 1, 2]
+    [a, b, a] = [1, 2, 1]
+    [a, b, a]
   end
 
   def patternMatching1() do
