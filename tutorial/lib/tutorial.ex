8d25931cfcbe213cc850b7221737be2ad6975bd8
momeemt <momiimt@gmail.com>
2020-04-29 07:09:54 +0900

    ✨ パターンマッチ(2.2)

--- tutorial/lib/tutorial.ex
+++ tutorial/lib/tutorial.ex
@@ -1,5 +1,16 @@
 defmodule Tutorial do
   def main([]) do
-    IO.puts("Hello world!")
+    IO.puts(patternMatching1)
+  end
+
+  def patternMatching1() do
+    a = [1, 2, 3]
+    a = 4
+    4 = a
+    # [a, b] = [1, 2, 3]
+    a = [[1, 2, 3]]
+    [a] = [[1, 2, 3]]
+    # [[a]] = [[1, 2, 3]]
+    a
   end
 end
