fb7adbe7b15b1c64a5afba9cc7cfde97ef99d6ba
momeemt <momiimt@gmail.com>
2020-04-30 07:15:13 +0900

    ✨ 簡単な高階関数の実装(5.3)

--- tutorial/lib/tutorial.ex
+++ tutorial/lib/tutorial.ex
@@ -3,23 +3,20 @@ defmodule Tutorial do
     list_concat = fn [a,b], [c,d] -> [a,b,c,d] end
     sum = fn a,b,c -> a + b + c end
     pair_tuple_to_list = fn {a, b} -> [a, b] end
-
     fizzbuzz = fn
       0, 0, _ -> "FizzBuzz"
       0, _, _ -> "Fizz"
       _, 0, _ -> "Buzz"
       _, _, third -> third
     end
-
     pass_fizzbuzz = fn n -> fizzbuzz.(rem(n,3), rem(n,5), n)  end
 
-    IO.inspect(pass_fizzbuzz.(10))
-    IO.inspect(pass_fizzbuzz.(11))
-    IO.inspect(pass_fizzbuzz.(12))
-    IO.inspect(pass_fizzbuzz.(13))
-    IO.inspect(pass_fizzbuzz.(14))
-    IO.inspect(pass_fizzbuzz.(15))
-    IO.inspect(pass_fizzbuzz.(16))
+    prefix = fn name -> (fn name2 -> "#{name} #{name2}" end) end
+
+    mrs = prefix.("Mrs")
+    IO.inspect(mrs.("Smith"))
+
+    IO.inspect(prefix.("Elixir").("Rocks"))
   end
 
   def patternMatching3() do
