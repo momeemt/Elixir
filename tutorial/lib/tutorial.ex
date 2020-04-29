bd68a7b5a2d138406b0fa0b2f0499be986bf9413
momeemt <momiimt@gmail.com>
2020-04-30 07:10:58 +0900

    ✨ パターンマッチを利用したFizzBuzzの実装(5.2)

--- tutorial/lib/tutorial.ex
+++ tutorial/lib/tutorial.ex
@@ -4,9 +4,22 @@ defmodule Tutorial do
     sum = fn a,b,c -> a + b + c end
     pair_tuple_to_list = fn {a, b} -> [a, b] end
 
-    IO.inspect(list_concat.([:a,:b], [:c,:d]))
-    IO.inspect(sum.(1,2,3))
-    IO.inspect(pair_tuple_to_list.({ 1234, 5678 }))
+    fizzbuzz = fn
+      0, 0, _ -> "FizzBuzz"
+      0, _, _ -> "Fizz"
+      _, 0, _ -> "Buzz"
+      _, _, third -> third
+    end
+
+    pass_fizzbuzz = fn n -> fizzbuzz.(rem(n,3), rem(n,5), n)  end
+
+    IO.inspect(pass_fizzbuzz.(10))
+    IO.inspect(pass_fizzbuzz.(11))
+    IO.inspect(pass_fizzbuzz.(12))
+    IO.inspect(pass_fizzbuzz.(13))
+    IO.inspect(pass_fizzbuzz.(14))
+    IO.inspect(pass_fizzbuzz.(15))
+    IO.inspect(pass_fizzbuzz.(16))
   end
 
   def patternMatching3() do
