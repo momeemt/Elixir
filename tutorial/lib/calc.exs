4854641f1aeaf415aca535013cb8d49b5c591e09
aq17 <aqneco@gmail.com>
2020-05-05 10:39:59 +0900

    ✨ リストと再帰/一般化 かなり難しかった...

--- tutorial/lib/calc.exs
+++ tutorial/lib/calc.exs
@@ -3,4 +3,33 @@ defmodule Calc do
   def sum([ head | tail ]) do
     head + sum(tail)
   end
+
+  def mapsum([], _), do: 0
+  def mapsum([head | tail], func) do
+    func.(head) + mapsum(tail, func)
+  end
+
+  def max([head | tail]) do
+    _max tail, head
+  end
+  defp _max([], maxval) do
+    maxval
+  end
+  defp _max([head | tail], maxval) when head >= maxval do
+    _max tail, head
+  end
+  defp _max([head | tail], maxval) when head < maxval do
+    _max tail, maxval
+  end
+
+  def ceasar([], _), do: []
+  def ceasar([head | tail], n) do
+    [_ceasar(head, n)] ++ ceasar(tail, n)
+  end
+  defp _ceasar(x, n) when (x + n) > ?z do
+    (x + n) - (?z - ?a + 1)
+  end
+  defp _ceasar(x, n) do
+    x + n
+  end
 end
