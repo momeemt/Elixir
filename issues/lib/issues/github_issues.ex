3867fe7dec278b32f70f03315eb2176f7a90d35b
aq17 <aqneco@gmail.com>
2020-05-09 06:50:24 +0900

    🐛 handle_responseにレスポンスがわたらないバグを修正

--- issues/lib/issues/github_issues.ex
+++ issues/lib/issues/github_issues.ex
@@ -3,7 +3,7 @@ defmodule Issues.GithubIssues do
 
   def fetch(user, project) do
     issues_url(user, project)
-    |> HTTPoison.get (@user_agent)
+    |> HTTPoison.get(@user_agent)
     |> handle_response
   end
 
@@ -11,11 +11,11 @@ defmodule Issues.GithubIssues do
     "https://api.github.com/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({ :ok, %{status_code: 200, body: body }}) do
+  def handle_response({:ok, %{status_code: 200, body: body, headers: _, request_url: _ }}) do
     { :ok, body }
   end
 
-  def handle_response({ _, %{status_code: _, body: body}}) do
+  def handle_response({ _, %{status_code: _, body: body, headers: _, request_url: _}}) do
     { :error, body }
   end
 end
