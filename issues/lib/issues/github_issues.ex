fba5ede8b983c44f311684da1bfda5f101b41e8e
aq17 <aqneco@gmail.com>
2020-05-09 07:51:16 +0900

    ✨ ログの生成(13.12)

--- issues/lib/issues/github_issues.ex
+++ issues/lib/issues/github_issues.ex
@@ -1,18 +1,25 @@
 defmodule Issues.GithubIssues do
+
+  require Logger
+
   @user_agent [ {"User-agent", "Elixir dave@pragprog.com"}]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
   end
 
   def issues_url(user, project) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
     "#{@github_url}}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({:ok, %{status_code: 200, body: body, headers: _, request_url: _ }}) do
+    Logger.error "Error #{status} returned"
     { :ok, Poison.Parser.parse!(body) }
   end
 
