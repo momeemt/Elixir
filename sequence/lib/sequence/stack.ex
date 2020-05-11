2c1a83201cd599ad321070e4b672011303fd76e3
aq17 <aqneco@gmail.com>
2020-05-12 07:01:22 +0900

    ✨ 一方通行の呼び出し(16.2)

--- sequence/lib/sequence/stack.ex
+++ sequence/lib/sequence/stack.ex
@@ -2,6 +2,10 @@ defmodule Sequence.Stack do
   use GenServer
 
   def handle_call(:pop, _from, [head | tail]) do
-    { :reply, head , tail }
+    { :reply, head, tail }
+  end
+
+  def handle_cast({:push, delta}, current_arr) do
+    { :noreply, [delta | current_arr] }
   end
 end
