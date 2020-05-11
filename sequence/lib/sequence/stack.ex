cb1780c08f311066a1b5a31d0c58aa005a8c8fd0
aq17 <aqneco@gmail.com>
2020-05-12 07:12:15 +0900

    ✨ インターフェイスを整理(16.5)

--- sequence/lib/sequence/stack.ex
+++ sequence/lib/sequence/stack.ex
@@ -1,11 +1,23 @@
 defmodule Sequence.Stack do
   use GenServer
 
+  def start_link(current_arr) do
+    GenServer.start_link(__MODULE__, current_arr, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call __MODULE__, :pop
+  end
+
+  def push(element) do
+    GenServer.cast __MODULE__, {:push, element}
+  end
+
   def handle_call(:pop, _from, [head | tail]) do
     { :reply, head, tail }
   end
 
-  def handle_cast({:push, delta}, current_arr) do
-    { :noreply, [delta | current_arr] }
+  def handle_cast({:push, element}, current_arr) do
+    { :noreply, [element | current_arr] }
   end
 end
